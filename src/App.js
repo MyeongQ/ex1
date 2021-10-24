8e08cd3cab61bf5c9a5418df60cc2ad8ac29b27e
kong <gmk0904@ajou.ac.kr>
2021-10-24 17:00:42 +0900

    create

--- src/App.js
+++ src/App.js
@@ -21,5 +21,19 @@ function App() {
     </div>
   );
 }
-// 보일러플레이트 만들기 : npx create-react-app 프로젝트 명
+
+/*
+  보일러플레이트 만들기 : npx create-react-app 프로젝트 명
+  git 생성 : $git init
+  git repository 생성 - 일반적으로 프로젝트 name과 동일하게 설정
+  remote repository 등록하기 : $git remote add origin "URL"
+  commit  하기 : 
+    $git add .  - commit할 파일 선택  // 다시 뺄때는 $git rm --cached "파일명"
+    $git commit -m "커밋 커멘션"
+  repository에 등록하기 : $git push origin master
+
+*/
+
+
+
 export default App;
